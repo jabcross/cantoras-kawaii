1be5e35edb5a7df0e89fa7746ae5b9e8b23b818b
Débora Prudêncio <debora.prudencio@gmail.com>
2021-10-10 16:27:38 -0300

    Add a final score screen
    - Level now ends at 43 seconds
    - Score label now also tracks total beats spawned and beats hit
    - Main redirects player to final score screen with dramatic reveal of scores!
    - Score is reset at the end of each level. TODO: retain score and display on level selection screen

--- Score.gd
+++ Score.gd
@@ -1,17 +1,19 @@
 extends Label
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-var value : int = 0
+var points : int = 0
+var total_beats : int = 0
+var beats_hit : int = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	text = String(value)
+	text = String(points)
+
+func add_beat():
+	total_beats += 1
 
 func _on_Middle_point(val):
-	value += val
-	if value < 0: value = 0
-	text = String(value)
+	if val == 100: beats_hit += 1
+	
+	points += val
+	if points < 0: points = 0
+	text = String(points)
---
+++ ScoreScreen.gd
@@ -0,0 +1,78 @@
+extends Control
+
+func _ready():
+	pass
+
+func reset_screen():
+	get_node("PodiumLeft/Baleia").hide()
+	get_node("PodiumLeft/Leoa").hide()
+	get_node("PodiumLeft/Morcegue").hide()
+	get_node("PodiumLeft/Sapa").hide()
+	$TotalBeats.hide()
+	$BeatsHit.hide()
+	$BeatsMissed.hide()
+	$FinalScore.hide()
+	$Grade.hide()
+	$Percentage.hide()
+	
+	get_node("PodiumLeft/Baleia/Sustain").show()
+	get_node("PodiumLeft/Leoa/Sustain").show()
+	get_node("PodiumLeft/Morcegue/Sustain").show()
+	get_node("PodiumLeft/Sapa/Sustain").show()
+	
+	get_node("PodiumLeft/Baleia/Error").hide()
+	get_node("PodiumLeft/Leoa/Error").hide()
+	get_node("PodiumLeft/Morcegue/Error").hide()
+	get_node("PodiumLeft/Sapa/Error").hide()
+
+func update_scores(score, total_beats, beats_hit, character):
+	
+	$TotalBeats.text = String(total_beats)
+	$BeatsHit.text = String(beats_hit)
+	$BeatsMissed.text = String(total_beats - beats_hit)
+	$FinalScore.text = String(score)
+	
+	var ideal_score = total_beats * 100
+	var percentage = stepify(float(score) / float(ideal_score), 0.01)
+	
+	$Percentage.text = String(percentage * 100) + "%"
+	
+	if percentage >= 0.9: $Grade.text = "S"
+	elif percentage >= 0.8: $Grade.text = "A"
+	elif percentage >= 0.7: $Grade.text = "B"
+	elif percentage >= 0.6: $Grade.text = "C"
+	elif percentage >= 0.5: $Grade.text = "D"
+	else:
+		$Grade.text = "F"
+		get_node("PodiumLeft/Baleia/Sustain").hide()
+		get_node("PodiumLeft/Leoa/Sustain").hide()
+		get_node("PodiumLeft/Morcegue/Sustain").hide()
+		get_node("PodiumLeft/Sapa/Sustain").hide()
+		
+		get_node("PodiumLeft/Baleia/Error").show()
+		get_node("PodiumLeft/Leoa/Error").show()
+		get_node("PodiumLeft/Morcegue/Error").show()
+		get_node("PodiumLeft/Sapa/Error").show()
+	
+	if character == "Baleia": get_node("PodiumLeft/Baleia").show()
+	elif character == "Leoa": get_node("PodiumLeft/Leoa").show()
+	elif character == "Morcegue": get_node("PodiumLeft/Morcegue").show()
+	elif character == "Sapa": get_node("PodiumLeft/Sapa").show()
+	
+	dramatic_reveal()
+	
+func dramatic_reveal():
+	yield(get_tree().create_timer(1), "timeout")
+	$TotalBeats.show()
+	yield(get_tree().create_timer(1), "timeout")
+	$BeatsHit.show()
+	yield(get_tree().create_timer(1), "timeout")
+	$BeatsMissed.show()
+	yield(get_tree().create_timer(1), "timeout")
+	$FinalScore.show()
+	yield(get_tree().create_timer(1), "timeout")
+	$Grade.show()
+	$Percentage.show()
+
+func _on_Button_pressed():
+	get_parent().return_to_title(self)
